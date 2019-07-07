5a65889a968887ac409b571fc36f9328f65c0703
conewaffle <kevinjm.liang@gmail.com>
2019-07-07 18:25:49 +1000

    imported icons into library

--- app/src/main/java/com/example/mydegree/MainActivity.java
+++ app/src/main/java/com/example/mydegree/MainActivity.java
@@ -80,17 +80,13 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.menusearch) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.menuplan) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.menuprofile) {
 
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.menuprogram) {
 
         }
 
